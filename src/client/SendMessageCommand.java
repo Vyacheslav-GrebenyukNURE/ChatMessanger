44b688d31503c47f8dd5012f433baa031dc3f2c9
Vyacheslav-GrebenyukNURE <vyacheslav.grebenyuk@nure.ua>
2017-04-21 15:35:31 +0300

    Rename main client class

--- src/client/SendMessageCommand.java
+++ src/client/SendMessageCommand.java
@@ -1,16 +1,23 @@
 package client;
 
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
 import java.util.Calendar;
 import java.util.concurrent.atomic.AtomicInteger;
 
 import logic.Message;
 
 public class SendMessageCommand implements Command {
-    ChatMessangerAppl parent;
-    ChatPanelView view;
-    AtomicInteger id;
+    private ChatMessengerAppl parent;
+    private ChatPanelView view;
+    private AtomicInteger id;
+    private final int PORT = 7070;
 
-    public SendMessageCommand(ChatMessangerAppl parent, ChatPanelView view, AtomicInteger id) {
+    public SendMessageCommand(ChatMessengerAppl parent, ChatPanelView view, AtomicInteger id) {
         this.parent = parent;
         this.view = view;
         this.id = id;
@@ -18,11 +25,11 @@ public class SendMessageCommand implements Command {
 
     @Override
     public void execute() {
-      // отправить сообщение на сервер
-      parent.getModel().addMessage(
-              new Message(Long.valueOf(id.incrementAndGet()), view.getTextMessageField().getText(), parent.getModel().getCurrentUser(), "", Calendar.getInstance()));
-      // получить обновления и вызвать нотификацию
-      view.getMessagesTextPane().setText(parent.getModel().getMessages());
-      view.getTextMessageField().setText("");
+        // отправить сообщение на сервер
+        parent.getModel().addMessage(new Message(Long.valueOf(id.incrementAndGet()),
+                view.getTextMessageField().getText(), parent.getModel().getCurrentUser(), "", Calendar.getInstance()));
+        // получить обновления и вызвать нотификацию
+        view.getMessagesTextPane().setText(parent.getModel().getMessages());
+        view.getTextMessageField().setText("");
     }
 }
